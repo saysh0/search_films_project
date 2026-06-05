b823966b2ead1ae359b7b2ae47a6cf1043d89d57
saysh0 <nikitaormandzhan@gmai.com>
2026-06-05 10:13:00 +0200

    project update1

--- db/sql_client.py
+++ db/sql_client.py
@@ -16,7 +16,14 @@ class SQL_DB:
         Enters the runtime context and establishes the database connection.
         :return: The instance of the SQL_DB class itself.
         """
-        self.conn = pymysql.connect(**self.config)
+        try:
+            self.conn = pymysql.connect(**self.config)
+        except pymysql.MySQLError as e:
+            print("╔" + "═" * 60 + "╗")
+            print(f"║{"[Ошибка соединения] Не удалось подключиться к базе данных.".center(60) + '║'}")
+            print("╚" + "═" * 60 + "╝")
+            print(f"Проверьте интернет-соединение или параметры подключения.Детали: {e}")
+            self.conn = None
         return self
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> bool:
@@ -27,6 +34,21 @@ class SQL_DB:
         :param exc_tb: The traceback object associated with the exception, if any.
         :return: False to allow any raised exceptions to propagate normally.
         """
+        if exc_type and issubclass(exc_type, pymysql.MySQLError):
+            print("╔" + "═" * 100 + "╗")
+            print(f"║{"[Ошибка сети] Соединение с базой данных было разорвано во время выполнения запроса.".center(100) + '║'}")
+            print("╚" + "═" * 100 + "╝")
+            print(f"Детали ошибки: {exc_val}\n")
+            if self.conn:
+                try:
+                    self.conn.rollback()
+                except pymysql.MySQLError:
+                    pass
+                finally:
+                    self.conn.close()
+                    self.conn = None
+            return True
+
         if self.conn:
             try:
                 if exc_type:
@@ -38,13 +60,17 @@ class SQL_DB:
                 self.conn = None
         return False
 
-    def select(self, query: str, params: Optional[Union[tuple, list, dict]] = None) -> List[Dict[str, Any]]:
+    def select(self, query: str, params: Optional[Union[tuple, list, dict]] = None) -> List[Dict[str, Any]] | None:
         """
         Executes a SELECT SQL query and returns all fetched rows.
         :param query: The SQL query string to be executed.
         :param params: Optional query parameters to safely interpolate into the SQL statement.
         :return: A list of dictionaries representing the fetched rows from the database.
         """
+        if not self.conn:
+            return None
+
         with self.conn.cursor() as cursor:
             cursor.execute(query, params)
             return cursor.fetchall()
+
